2ea52cb53f55849f81fac2f6180d498a15fab3d5
montes8 <emontesinos812@gmail.com>
2018-07-08 11:12:24 -0500

    Implementando animacion al Splash

--- app/src/main/java/com/example/montes8/demosqliteroomjava/SplashActivity.java
+++ app/src/main/java/com/example/montes8/demosqliteroomjava/SplashActivity.java
@@ -2,12 +2,29 @@ package com.example.montes8.demosqliteroomjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 
 public class SplashActivity extends AppCompatActivity {
+
+    LinearLayout once,doce;
+    Animation arriba,abajo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_spalsh);
+        setContentView(R.layout.activity_splash);
+
+        once =(LinearLayout) findViewById(R.id.once);
+        doce = (LinearLayout) findViewById(R.id.doce);
+
+        arriba = AnimationUtils.loadAnimation(this,R.anim.animacion);
+        abajo = AnimationUtils.loadAnimation(this,R.anim.animacion_botton);
+
+        once.setAnimation(arriba);
+        doce.setAnimation(abajo);
+
+
     }
 }
