a149fcb38f53accbb1e7886580f64e21c236269e
montes8 <emontesinos812@gmail.com>
2018-07-08 11:50:17 -0500

    creando modelo de usuario

--- app/src/main/java/com/example/montes8/demosqliteroomjava/model/Usuario.java
+++ app/src/main/java/com/example/montes8/demosqliteroomjava/model/Usuario.java
@@ -1,4 +1,64 @@
 package com.example.montes8.demosqliteroomjava.model;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity
 public class Usuario {
+
+    @PrimaryKey(autoGenerate = true)
+    private Long idUsu;
+    private String nombre;
+    private String nombreUsuario;
+    private String contrasenia;
+    private String pais;
+
+
+    public Usuario(Long idUsu, String nombre, String nombreUsuario, String contrasenia, String pais) {
+        this.idUsu = idUsu;
+        this.nombre = nombre;
+        this.nombreUsuario = nombreUsuario;
+        this.contrasenia = contrasenia;
+        this.pais = pais;
+    }
+
+    public Long getIdUsu() {
+        return idUsu;
+    }
+
+    public void setIdUsu(Long idUsu) {
+        this.idUsu = idUsu;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
 }
