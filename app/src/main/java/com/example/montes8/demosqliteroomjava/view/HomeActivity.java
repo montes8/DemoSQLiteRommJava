52db52215a771f6842b29d9c0eafabe54c76b7be
montes8 <emontesinos812@gmail.com>
2018-07-13 16:51:27 -0500

    implementando toolbar

--- app/src/main/java/com/example/montes8/demosqliteroomjava/view/HomeActivity.java
+++ app/src/main/java/com/example/montes8/demosqliteroomjava/view/HomeActivity.java
@@ -2,6 +2,7 @@ package com.example.montes8.demosqliteroomjava.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.montes8.demosqliteroomjava.R;
 
@@ -11,6 +12,14 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ajustarToolbarHome();
 
+    }
+
+    public void ajustarToolbarHome(){
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.homeToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Lista de Platos");
     }
 }
