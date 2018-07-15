19d149fdb87122b9b5c09f522ce88ab700c80fde
montes8 <emontesinos812@gmail.com>
2018-07-15 10:32:34 -0500

    agregando toolbar

--- app/src/main/java/com/example/montes8/demosqliteroomjava/view/DetallePedidoActivity.java
+++ app/src/main/java/com/example/montes8/demosqliteroomjava/view/DetallePedidoActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.example.montes8.demosqliteroomjava.R;
 import com.example.montes8.demosqliteroomjava.adapter.DetalleAdapter;
@@ -16,19 +18,35 @@ public class DetallePedidoActivity extends AppCompatActivity {
 
     RecyclerView detalleRecyclerView;
     DetalleAdapter detalleAdapter;
+    Toolbar toolbardetalle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_pedido);
         detalleRecyclerView = findViewById(R.id.pedido_recyclerview);
+        toolbardetalle = (Toolbar) findViewById(R.id.detalleToolbar);
+
+        ajustarToolbardetallepedido();
+
         detalleRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         detalleAdapter = new DetalleAdapter(this);
-
         ArrayList<DetalleTemporal> lista = OrdenTemporal.optenerorden();
-
         detalleAdapter.addList(lista);
         detalleRecyclerView.setAdapter(detalleAdapter);
 
+    }
+
+    private void ajustarToolbardetallepedido(){
+
+        setSupportActionBar(toolbardetalle);
+        getSupportActionBar().setTitle("Detalle de Ordenes");
+        toolbardetalle.setNavigationIcon(R.drawable.ic_atras);
+        toolbardetalle.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
